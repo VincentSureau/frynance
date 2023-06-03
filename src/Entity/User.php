c3abe0ebcdace9d423a5a3886bd64cb7e9be59b3
vincent sureau <vincentsureau5@gmail.com>
2023-06-03 19:17:29 +0200

    added test for user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,12 +2,15 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use App\Repository\UserRepository;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity('email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -15,6 +18,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\Email]
     #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
@@ -24,12 +28,15 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @var string The hashed password
      */
+    #[Assert\NotBlank]
     #[ORM\Column]
     private ?string $password = null;
 
+    #[Assert\NotBlank]
     #[ORM\Column(length: 255)]
     private ?string $firstname = null;
 
+    #[Assert\NotBlank]
     #[ORM\Column(length: 255)]
     private ?string $lastname = null;
 
--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -8,6 +8,20 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class UserTest extends KernelTestCase
 {
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
+
+    protected function setUp(): void
+    {
+        $kernel = self::bootKernel();
+
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
     public function getEntity(): User
     {
         return (new User())
@@ -35,12 +49,43 @@ class UserTest extends KernelTestCase
         $this->assertHasErrors($this->getEntity(), 0);
     }
 
-    public function testSomething(): void
+    public function testInvalidEmail()
     {
-        $kernel = self::bootKernel();
+        $this->assertHasErrors($this->getEntity()->setEmail('wrongmail'), 1);
+    }
 
-        $this->assertSame('test', $kernel->getEnvironment());
-        // $routerService = static::getContainer()->get('router');
-        // $myCustomService = static::getContainer()->get(CustomService::class);
+    public function testInvalidBlankLastName()
+    {
+        $this->assertHasErrors($this->getEntity()->setLastname(''), 1);
+    }
+
+    public function testInvalidBlankPassword()
+    {
+        $this->assertHasErrors($this->getEntity()->setPassword(''), 1);
+    }
+
+    public function testInvalidUniqueEmail()
+    {
+        $user1 = $this->getEntity();
+        $this->entityManager->persist($user1);
+        $this->entityManager->flush();
+        $this->assertHasErrors($this->getEntity(), 1);
+    }
+    // public function testSomething(): void
+    // {
+    //     $kernel = self::bootKernel();
+
+    //     $this->assertSame('test', $kernel->getEnvironment());
+    //     // $routerService = static::getContainer()->get('router');
+    //     // $myCustomService = static::getContainer()->get(CustomService::class);
+    // }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        // doing this is recommended to avoid memory leaks
+        $this->entityManager->close();
+        $this->entityManager = null;
     }
 }
