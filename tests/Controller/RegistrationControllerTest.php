ad72c5a1211b5ea4f667f2f91fdae986c88157bb
vincent sureau <vincentsureau5@gmail.com>
2023-06-04 20:55:03 +0200

    added test for registration controller

---
+++ tests/Controller/RegistrationControllerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RegistrationControllerTest extends WebTestCase
+{
+    public function testRegistrationPageIsDisplayed()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/fr/register');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Sign Up');
+    }
+
+    public function testNewValidUser(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/fr/register');
+        $buttonCrawlerNode = $crawler->selectButton('Sign Up');
+
+        $form = $buttonCrawlerNode->form();
+        $form['registration_form[email]'] = 'test@example.com';
+        $form['registration_form[firstname]'] = 'firstname';
+        $form['registration_form[lastname]'] = 'lastname';
+        $form['registration_form[plainPassword][first]'] = "password";
+        $form['registration_form[plainPassword][second]'] = "password";
+        $form['registration_form[agreeTerms]']->tick();
+        $client->submit($form);
+
+        $this->assertResponseRedirects('/fr/', Response::HTTP_FOUND);
+
+        /** @var UserRepository $userRepository */
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        /** @var \App\Entity\Post $post */
+        $user = $userRepository->findOneByEmail('test@example.com');
+
+        $this->assertNotNull($user);
+        $this->assertSame('firstname', $user->getFirstName());
+        $this->assertSame('lastname', $user->getLastname());
+    }
+
+    public function testNewInvalidUser(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/fr/register');
+        $buttonCrawlerNode = $crawler->selectButton('Sign Up');
+
+        $form = $buttonCrawlerNode->form();
+        $form['registration_form[email]'] = 'test';
+        $form['registration_form[firstname]'] = '';
+        $form['registration_form[lastname]'] = '';
+        $form['registration_form[plainPassword][first]'] = "password";
+        $form['registration_form[plainPassword][second]'] = "password2";
+        $client->submit($form);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('#registration_form_email.is-invalid ~ .invalid-feedback');
+        $this->assertSelectorExists('#registration_form_firstname.is-invalid ~ .invalid-feedback');
+        $this->assertSelectorExists('#registration_form_lastname.is-invalid ~ .invalid-feedback');
+        $this->assertSelectorExists('#registration_form_plainPassword_first.is-invalid ~ .invalid-feedback');
+    }
+}
