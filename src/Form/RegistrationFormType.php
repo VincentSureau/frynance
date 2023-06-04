55987405739a71d141183aeed7f63d1a739ca256
vincent sureau <vincentsureau5@gmail.com>
2023-06-04 09:50:50 +0200

    added registration form

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control form-control-xl',
+                    'placeholder' => 'Email'
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control form-control-xl',
+                    'placeholder' => 'Firstname'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control form-control-xl',
+                    'placeholder' => 'Lastname'
+                ]
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'attr' => [
+                    'class' => 'form-check-input me-2'
+                ],
+                'label_attr' => [
+                    'class' => 'form-check-label text-gray-600'
+                ],
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => [
+                    'attr' => ['class' => 'password-field']
+                ],
+                'required' => true,
+                'mapped' => false,
+                'first_options'  => [
+                    'label' => 'Password',
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                        'class' => 'form-control form-control-xl',
+                        'placeholder' => 'Password'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password',
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                        'class' => 'form-control form-control-xl',
+                        'placeholder' => 'Confirm Password'
+                    ]
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'validation_groups' => ['registration'],
+        ]);
+    }
+}
