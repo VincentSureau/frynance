f6c208fa94092be493a705a330d8a56e343ee38d
Vincent Sureau <vincentsureau5@gmail.com>
2023-06-05 14:30:54 +0200

    added tests for ResetPasswordController

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher)
+    {}
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user
+            ->setFirstname('firtname')
+            ->setLastname('lastname')
+            ->setEmail('name@example.com')
+            ->setPassword(
+                $this->userPasswordHasher->hashPassword(
+                    $user,
+                    'password'
+                )
+            )
+        ;
+        $manager->persist($user);
+        $manager->flush();
+        $this->addReference('user', $user);
+    }
+}
