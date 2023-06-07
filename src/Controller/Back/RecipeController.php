646f4213ab73feb2a5e44b9479887def1e17af24
Vincent Sureau <vincentsureau5@gmail.com>
2023-06-07 15:37:17 +0200

    added base CRUD for recipe

---
+++ src/Controller/Back/RecipeController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller\Back;
+
+use App\Entity\Recipe;
+use App\Form\RecipeType;
+use App\Repository\RecipeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/recipe')]
+class RecipeController extends AbstractController
+{
+    #[Route('/', name: 'recipe_index', methods: ['GET'])]
+    public function index(RecipeRepository $recipeRepository): Response
+    {
+        return $this->render('back/recipe/index.html.twig', [
+            'recipes' => $recipeRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'recipe_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, RecipeRepository $recipeRepository): Response
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $recipe->setUser($this->getUser());
+            $recipeRepository->save($recipe, true);
+
+            return $this->redirectToRoute('app_admin_recipe_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('back/recipe/new.html.twig', [
+            'recipe' => $recipe,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'recipe_show', methods: ['GET'])]
+    public function show(Recipe $recipe): Response
+    {
+        return $this->render('back/recipe/show.html.twig', [
+            'recipe' => $recipe,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'recipe_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Recipe $recipe, RecipeRepository $recipeRepository): Response
+    {
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $recipeRepository->save($recipe, true);
+
+            return $this->redirectToRoute('app_admin_recipe_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('back/recipe/edit.html.twig', [
+            'recipe' => $recipe,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'recipe_delete', methods: ['POST'])]
+    public function delete(Request $request, Recipe $recipe, RecipeRepository $recipeRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$recipe->getId(), $request->request->get('_token'))) {
+            $recipeRepository->remove($recipe, true);
+        }
+
+        return $this->redirectToRoute('app_admin_recipe_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
