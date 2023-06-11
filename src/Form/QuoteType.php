379aa6d8a82800abf1e7e43739adf2df16d9b99f
vincent sureau <vincentsureau5@gmail.com>
2023-06-11 10:48:24 +0200

    generated CRUD for quotes

---
+++ src/Form/QuoteType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Quote;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QuoteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('price')
+            ->add('title')
+            ->add('user')
+            ->add('recipes')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Quote::class,
+        ]);
+    }
+}
