8f67ab03402e136490bb95227a6c7ab7ae629952
Vincent Sureau <vincentsureau5@gmail.com>
2023-06-13 14:30:55 +0200

    wip: quote creator and quote line item creator component

--- src/Twig/Components/QuoteCreatorComponent.php
+++ src/Twig/Components/QuoteCreatorComponent.php
@@ -3,10 +3,14 @@
 namespace App\Twig\Components;
 
 use App\Entity\Quote;
+use Symfony\UX\LiveComponent\Attribute\LiveArg;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\LiveListener;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\ValidatableComponentTrait;
+use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[AsLiveComponent('quote_creator')]
@@ -21,8 +25,67 @@ final class QuoteCreatorComponent extends AbstractController
     #[LiveProp]
     public array $quoteLineItems = [];
 
+    /**
+     * A temporary flag that we just saved.
+     *
+     * This doesn't need to be a LiveProp because it's set in a LiveAction,
+     * rendered immediately, then we want it to be forgotten.
+     */
+    public bool $savedSuccessfully = false;
+    public bool $saveFailed = false;
+
     public function mount(Quote $quote): void
     {
         $this->quote = $quote;
+    }
+
+    #[LiveAction]
+    public function addQuoteLineItem(): void
+    {
+        $this->quoteLineItems[] = [
+            'quoteLineId' => null,
+            'recipeId' => null,
+            'quantity' => 1,
+            'preparation' => 1,
+            'unit_price' => 1,
+            'total_price' => 0,
+            'isEditing' => true,
+        ];
+    }
+
+    #[ExposeInTemplate()]
+    public function getTotal(): float
+    {
+        $total = 0;
+
+        foreach ($this->quoteLineItems as $quotelineItem) {
+            $total += $quotelineItem['total_price'];
+        }
+
+        return $total;
+    }
+
+    #[LiveListener('removeQuoteLineItem')]
+    public function removeQuoteLineItem(#[LiveArg] int $key): void
+    {
+        unset($this->quoteLineItems[$key]);
+    }
+
+    #[ExposeInTemplate]
+    public function areAnyLineItemsEditing(): bool
+    {
+        foreach ($this->quoteLineItems as $lineItem) {
+            if ($lineItem['isEditing']) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    #[LiveListener('quote_line_item:change_edit_mode')]
+    public function onQuoteLineItemEditModeChange(#[LiveArg] int $key, #[LiveArg] $isEditing): void
+    {
+        $this->quoteLineItems[$key]['isEditing'] = $isEditing;
     }
 }
---
+++ src/Twig/Components/QuoteCreatorLineItemComponent.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Twig\Components;
+
+use App\Entity\Recipe;
+use App\Entity\QuoteLine;
+use App\Repository\RecipeRepository;
+use App\Repository\QuoteLineRepository;
+use Symfony\UX\LiveComponent\LiveResponder;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\ValidatableComponentTrait;
+use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
+
+#[AsLiveComponent('quote_creator_line_item')]
+class QuoteCreatorLineItemComponent
+{
+    use DefaultActionTrait;
+    use ValidatableComponentTrait;
+
+    #[LiveProp]
+    public int $key;
+
+    #[LiveProp(writable: true)]
+    public ?QuoteLine $quoteLine = null;
+
+    #[LiveProp(writable: true)]
+    #[Assert\NotNull]
+    public ?Recipe $recipe = null;
+
+    #[LiveProp(writable: true)]
+    public ?int $recipeId = null;
+
+    #[LiveProp(writable: true)]
+    #[Assert\Positive]
+    public float $quantity = 1;
+
+    #[LiveProp(writable: true)]
+    #[Assert\Positive]
+    public float $preparation = 1;
+
+    #[LiveProp(writable: true)]
+    #[Assert\Positive]
+    public float $unit_price = 1;
+
+    #[LiveProp(writable: true)]
+    #[Assert\PositiveOrZero]
+    public float $total_price = 0;
+
+    #[LiveProp()]
+    public bool $isEditing = false;
+
+    public function __construct(private RecipeRepository $recipeRepository, private QuoteLineRepository $quoteLineRepository)
+    {
+    }
+
+    public function mount(?int $quoteLineId, ?int $recipeId): void
+    {
+        if ($quoteLineId) {
+            $this->quoteLine = $this->quoteLineRepository->find($quoteLineId);
+        }
+        if ($recipeId) {
+            $this->recipe = $this->recipeRepository->find($recipeId);
+        }
+    }
+
+    #[LiveAction]
+    public function save(LiveResponder $responder): void
+    {
+        $this->validate();
+        $responder->emitUp('line_item:save', [
+            'key' => $this->key,
+            'recipeId' => $this->recipe->getId(),
+            'quantity' => $this->quantity,
+            'unit_price' => $this->unit_price,
+            'quantity' => $this->quantity,
+            'preparation' => $this->preparation,
+            'total_price' => $this->total_price,
+            'quoteLineId' => $this->quoteLine?->getId(),
+        ]);
+
+        $this->changeEditMode(false, $responder);
+    }
+
+    #[LiveAction]
+    public function edit(LiveResponder $responder): void
+    {
+        $this->changeEditMode(true, $responder);
+    }
+
+    #[ExposeInTemplate]
+    public function getRecipes(): array
+    {
+        return $this->recipeRepository->findAll();
+    }
+
+    public function getTotalPrice()
+    {
+        try {
+            if($this->quantity == 0) {
+                $this->total_price = 0;
+                throw new \Exception("Division by Zero");
+            }
+            $this->total_price = $this->unit_price * $this->quantity;
+        } catch (\Exception $e) {
+            $this->total_price = 0;
+        }
+        return $this->total_price;
+    }
+
+    private function changeEditMode(bool $isEditing, LiveResponder $responder): void
+    {
+        $this->isEditing = $isEditing;
+
+        // emit to QuoteCreator so it can track which items are being edited
+        $responder->emitUp('quote_line_item:change_edit_mode', [
+            'key' => $this->key,
+            'isEditing' => $this->isEditing,
+        ]);
+    }
+}
