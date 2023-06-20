99b3b71472cf40d7b6206af67df35ac291d402c4
Vincent Sureau <vincentsureau5@gmail.com>
2023-06-20 20:41:58 +0200

    added security annotation on profile controler

--- src/Controller/Back/ProfileController.php
+++ src/Controller/Back/ProfileController.php
@@ -8,11 +8,11 @@ use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
+#[IsGranted('ROLE_USER')]
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'profile')]
