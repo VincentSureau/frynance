a3aa1a439137a21d1642d9f5819f33f75631b589
vincent sureau <vincentsureau5@gmail.com>
2023-06-22 19:39:39 +0200

    added translation domain on form

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -52,6 +52,8 @@ class ChangePasswordFormType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([]);
+        $resolver->setDefaults([
+            'translation_domain' => 'auth'
+        ]);
     }
 }
